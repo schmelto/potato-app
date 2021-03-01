6f98e19b2c030ba254152b4de0e6b65059011502
schmelto <tom.schmelzer@web.de>
2021-03-01 19:22:27 +0100

    clean up code (#2)

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-
 import { ModalController } from '@ionic/angular';
-import { RecipePage } from '../modal/recipe.page';
-
 import { Router, NavigationExtras } from '@angular/router';
+import { RecipePage } from '../modal/recipe.page';
 
 @Component({
   selector: 'app-home',
@@ -14,18 +12,17 @@ export class HomePage {
 
   potatoes;
 
-  constructor(public modalController: ModalController, private router: Router) {
-  }
+  constructor(public modalController: ModalController, private router: Router) { }
+
   async presentModal() {
     if (this.potatoes >= 0) {
-    const modal = await this.modalController.create({
-      component: RecipePage,
-      componentProps: { 
-        potatoes: this.potatoes, // pass this value to modal
-      }
-    });
-    return await modal.present();
+      const modal = await this.modalController.create({
+        component: RecipePage,
+        componentProps: { 
+          potatoes: this.potatoes, // pass this value to modal
+        }
+      });
+      return await modal.present();
+    }
   }
-  }
-
 }
