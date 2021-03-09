9308db7fead257a35fa5b9e697d6cd71841d563e
schmelto <tom.schmelzer@web.de>
2021-03-09 18:55:12 +0100

    add menu (#12)
    * add language swich dropdown
    
    * remove buttons form home.page.html
    
    * remove unused language swich functions in home.page.ts

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -8,12 +8,25 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AppComponent {
 
+  public selectedLanguage: string = 'en';
+
+  languages = [
+    { id: 1, value: 'de', name: "DE" },
+    { id: 2, value: 'en', name: "EN" },
+    // here you can enter some more languages. Note that you have to add coresponding i18n files.
+  ];
+
   constructor(private translate: TranslateService) {
-      translate.setDefaultLang('en');
+      translate.setDefaultLang(this.selectedLanguage);
   }
 
   useLanguage(language: string): void {
     this.translate.use(language);
   }
-  
+
+  changeLanguage(data) {
+    this.selectedLanguage = data.detail.value;
+    this.useLanguage(this.selectedLanguage);
+  }
+
 }
--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Router, NavigationExtras } from '@angular/router';
 import { RecipePage } from '../modal/recipe.page';
-import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +12,7 @@ export class HomePage {
   
   potatoes;
 
-  constructor(public modalController: ModalController, private router: Router, private app: AppComponent) {
+  constructor(public modalController: ModalController, private router: Router) {
    }
 
   async presentModal() {
@@ -26,10 +25,6 @@ export class HomePage {
       });
       return await modal.present();
     }
-  }
-
-  useLanguage(language: string): void {
-    this.app.useLanguage(language);
   }
 
 }
